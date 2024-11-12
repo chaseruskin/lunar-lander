d27c5102afce54278d2d4048906d57b60dc2df5c
Nihal Simha <nihal.simha19@imperial.ac.uk>
2024-11-11 22:38:33 -0500

    naming weight variables correctly to access them from saved .pth file

--- src/models.py
+++ src/models.py
@@ -66,7 +66,7 @@ class DQN(Model):
         self.layer1 = nn.Linear(n_observations, 128)
         self.layer2 = nn.Linear(128, 128)
         # self.layer3 = nn.ReLU()
-        self.layer4 = nn.Linear(128, n_actions)
+        self.layer3 = nn.Linear(128, n_actions)
         pass
          
     def forward(self, x):
@@ -78,7 +78,7 @@ class DQN(Model):
         x = F.relu(self.layer1(x))
         x = F.relu(self.layer2(x))
         # x = F.relu(self.layer3(x))
-        return self.layer4(x)
+        return self.layer3(x)
     
     pass
 
